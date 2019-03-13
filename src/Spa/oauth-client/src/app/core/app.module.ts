cd2a852190b37326b8c53f36a73950397999afe6
mmacneil <markmacneil@gmail.com>
2019-03-13 11:58:52 -0300

    wip

--- src/Spa/oauth-client/src/app/core/app.module.ts
+++ src/Spa/oauth-client/src/app/core/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Module Imports */
+import { AccountModule }  from '../modules/account/account.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AccountModule,
     BrowserModule,
     AppRoutingModule
   ],
