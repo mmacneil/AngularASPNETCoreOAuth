9864f634e37bb8ead28de6424bcb127f0f2b92b7
mmacneil <markmacneil@gmail.com>
2019-03-13 21:55:35 -0300

    wip

--- src/Spa/oauth-client/e2e/src/app.e2e-spec.ts
+++ src/Spa/oauth-client/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display header title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to oauth-client!');
+    expect(page.getHeaderTitleText()).toEqual('OAuth Client');
   });
 
   afterEach(async () => {
--- src/Spa/oauth-client/e2e/src/app.po.ts
+++ src/Spa/oauth-client/e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  getHeaderTitleText() {
+    return element(by.css('app-root > app-header strong')).getText() as Promise<string>;
   }
 }
