16c679c8147cd59422628d367add7b490c51c929
mmacneil <markmacneil@gmail.com>
2019-03-19 20:13:42 -0300

    wip

--- src/AuthServer/AuthServer.Api/Config.cs
+++ src/AuthServer/AuthServer.Api/Config.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using AuthServer.Infrastructure.Constants;
-using IdentityServer4;
 using IdentityServer4.Models;
 
 namespace AuthServer.Api
@@ -21,36 +20,24 @@ namespace AuthServer.Api
         {
             return new List<ApiResource>
             {
-                new ApiResource(Roles.Consumer, "Aycoutay API")
+                new ApiResource(Roles.Consumer, "Resource API")
             };
         }
 
         public static IEnumerable<Client> GetClients()
         {
-            // client credentials client
-            return new List<Client>
+            return new[]
             {
-                // resource owner password grant client
-                new Client
-                {
-                    ClientId = "ro.aycoutay-mobile",
-                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-
-                    ClientSecrets =
-                    {
-                        new Secret("secret".Sha256())
-                    },
-                    AllowedScopes = {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        IdentityServerConstants.StandardScopes.Email,
-                        IdentityServerConstants.StandardScopes.Address,
-                        Roles.Consumer
-                    },
-                    AllowOfflineAccess = true,
-                    // Access token life time is 90 days
-                    AccessTokenLifetime = 7776000,
-                    AbsoluteRefreshTokenLifetime = 10368000  // 120 days
+                new Client {
+                    ClientId = "angular_spa",
+                    ClientName = "Angular SPA",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes = { "openid", "profile", "email", "api1.read" },
+                    RedirectUris = {"http://localhost:4200/auth-callback"},
+                    PostLogoutRedirectUris = {"http://localhost:4200/"},
+                    AllowedCorsOrigins = {"http://localhost:4200"},
+                    AllowAccessTokensViaBrowser = true,
+                    AccessTokenLifetime = 3600
                 }
             };
         }
