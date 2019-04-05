56d427737c2bcecb9724723713cdee7fa9604e16
mmacneil <markmacneil@gmail.com>
2019-04-05 13:37:38 -0300

    Remove cruft.

--- src/Resource.Api/Resource.Api/Controllers/ValuesController.cs
+++ src/Resource.Api/Resource.Api/Controllers/ValuesController.cs
@@ -17,30 +17,5 @@ namespace Resource.Api.Controllers
         {
             return new JsonResult(User.Claims.Select(c => new { c.Type, c.Value }));
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
